af4783bf3024fd3acf419ddece181e08bca60ef4
Oleg Nemanov <lego12239@yandex.ru>
2023-01-18 19:43:01 +0300

    add html_escape and html_unescape
    html_escape proc is for escaping a special characters in a specified
    string according to html rules.
    
    html_unescape proc is for unescaping escape sequences in a specified
    string according to html rules.

--- util.tcl
+++ util.tcl
@@ -185,3 +185,132 @@ proc naddr_parse {naddr {defaddr 0.0.0.0}} {
 
 	return $addr
 }
+
+# Escape special characters according to HTML escape rules.
+# prms:
+#  str - a string to process
+# ret:
+#  STR - a string with special characters escaped
+# &, <, > - for any places
+# ", ' - for element attributes
+proc html_escape {str} {
+	set str [string map {& &amp; < &lt; > &gt; \" &quot; \' &#39;} $str]
+
+	return $str
+}
+
+# Unescape HTML escape sequences.
+# prms:
+#  str - a string to process
+# ret:
+#  STR - a string with unescaped escape sequences
+# "&#xX;", "&#N;" and "&W;" is converted to a character with hex code X or
+# decimal code N or name W.
+proc html_unescape {str} {
+	set i0 0
+	set res ""
+	set _html_unescape_escseq [dict create\
+	  quot {"} amp & lt > gt < Tab "\t" NewLine "\n" nbsp "\uA0"\
+	  iexcl "\uA1" cent "\uA2" pound "\uA3" curren "\uA4" yen "\uA5"\
+	  brvbar "\uA6" sect "\uA7" uml "\uA8" copy "\uA9" ordf "\uAA"\
+	  laquo "\uAB" not "\uAC" shy "\uAD" reg "\uAE" macr "\uAF"\
+	  deg "\uB0" plusmn "\uB1" sup2 "\uB2" sup3 "\uB3" acute "\uB4"\
+	  micro "\uB5" para "\uB6" dot "\uB7" cedil "\uB8" sup1 "\uB9"\
+	  ordm "\uBA" raquo "\uBB" frac14 "\uBC" frac12 "\uBD" frac34 "\uBE"\
+	  iquest "\uBF" Agrave "\uC0" Aacute "\uC1" Acirc "\uC2"\
+	  Atilde "\uC3" Auml "\uC4" Aring "\uC5" AElig "\uC6" Ccedil "\uC7"\
+	  Egrave "\uC8" Eacute "\uC9" Ecirc "\uCA" Euml "\uCB" Igrave "\uCC"\
+	  Iacute "\uCD" Icirc "\uCE" Iuml "\uCF" ETH "\uD0" Ntilde "\uD1"\
+	  Ograve "\uD2" Oacute "\uD3" Ocirc "\uD4" Otilde "\uD5" Ouml "\uD6"\
+	  times "\uD7" Oslash "\uD8" Ugrave "\uD9" Uacute "\uDA"\
+	  Ucirc "\uDB" Uuml "\uDC" Yacute "\uDD" THORN "\uDE" szlig "\uDF"\
+	  agrave "\uE0" aacute "\uE1" acirc "\uE2" atilde "\uE3" auml "\uE4"\
+	  aring "\uE5" aelig "\uE6" ccedil "\uE7" egrave "\uE8"\
+	  eacute "\uE9" ecirc "\uEA" euml "\uEB" igrave "\uEC" iacute "\uED"\
+	  icirc "\uEE" iuml "\uEF" eth "\uF0" ntilde "\uF1" ograve "\uF2"\
+	  oacute "\uF3" ocirc "\uF4" otilde "\uF5" ouml "\uF6" divide "\uF7"\
+	  oslash "\uF8" ugrave "\uF9" uacute "\uFA" ucirc "\uFB" uuml "\uFC"\
+	  yacute "\uFD" thorn "\uFE" yuml "\uFF" Amacr "\u0100"\
+	  amacr "\u0101" Abreve "\u0102" abreve "\u0103" Aogon "\u0104"\
+	  aogon "\u0105" Cacute "\u0106" cacute "\u0107" Ccirc "\u0108"\
+	  ccirc "\u0109" Cdot "\u010A" cdot "\u010B" Ccaron "\u010C"\
+	  ccaron "\u010D" Dcaron "\u010E" dcaron "\u010F" Dstrok "\u0110"\
+	  dstrok "\u0111" Emacr "\u0112" emacr "\u0113" Ebreve "\u0114"\
+	  ebreve "\u0115" Edot "\u0116" edot "\u0117" Eogon "\u0118"\
+	  eogon "\u0119" Ecaron "\u011A" ecaron "\u011B" Gcirc "\u011C"\
+	  gcirc "\u011D" Gbreve "\u011E" gbreve "\u011F" Gdot "\u0120"\
+	  gdot "\u0121" Gcedil "\u0122" gcedil "\u0123" Hcirc "\u0124"\
+	  hcirc "\u0125" Hstrok "\u0126" hstrok "\u0127" Itilde "\u0128"\
+	  itilde "\u0129" Imacr "\u012A" imacr "\u012B" Ibreve "\u012C"\
+	  ibreve "\u012D" Iogon "\u012E" iogon "\u012F" Idot "\u0130"\
+	  imath "\u0131" inodot "\u0131" IJlig "\u0132" ijlig "\u0133"\
+	  Jcirc "\u0134" jcirc "\u0135" Kcedil "\u0136" kcedil "\u0137"\
+	  kgreen "\u0138" Lacute "\u0139" lacute "\u013A" Lcedil "\u013B"\
+	  lcedil "\u013C" Lcaron "\u013D" lcaron "\u013E" Lmidot "\u013F"\
+	  lmidot "\u0140" Lstrok "\u0141" lstrok "\u0142" Nacute "\u0143"\
+	  nacute "\u0144" Ncedil "\u0145" ncedil "\u0146" Ncaron "\u0147"\
+	  ncaron "\u0148" napos "\u0149" ENG "\u014A" eng "\u014B"\
+	  Omacr "\u014C" omacr "\u014D" Obreve "\u014E" obreve "\u014F"\
+	  Odblac "\u0150" odblac "\u0151" OElig "\u0152" oelig "\u0153"\
+	  Racute "\u0154" racute "\u0155" Rcedil "\u0156" rcedil "\u0157"\
+	  Rcaron "\u0158" rcaron "\u0159" Sacute "\u015A" sacute "\u015B"\
+	  Scirc "\u015C" scirc "\u015D" Scedil "\u015E" scedil "\u015F"\
+	  Scaron "\u0160" scaron "\u0161" Tcedil "\u0162" tcedil "\u0163"\
+	  Tcaron "\u0164" tcaron "\u0165" Tstrok "\u0166" tstrok "\u0167"\
+	  Utilde "\u0168" utilde "\u0169" Umacr "\u016A" umacr "\u016B"\
+	  Ubreve "\u016C" ubreve "\u016D" Uring "\u016E" uring "\u016F"\
+	  Udblac "\u0170" udblac "\u0171" Uogon "\u0172" uogon "\u0173"\
+	  Wcirc "\u0174" wcirc "\u0175" Ycirc "\u0176" ycirc "\u0177"\
+	  Yuml "\u0178" fnof "\u0192" circ "\u02C6" tilde "\u02DC"\
+	  Alpha "\u0391" Beta "\u0392" Gamma "\u0393" Delta "\u0394"\
+	  Epsilon "\u0395" Zeta "\u0396" Eta "\u0397" Theta "\u0398"\
+	  Iota "\u0399" Kappa "\u039A" Lambda "\u039B" Mu "\u039C"\
+	  Nu "\u039D" Xi "\u039E" Omicron "\u039F" Pi "\u03A0" Rho "\u03A1"\
+	  Sigma "\u03A3" Tau "\u03A4" Upsilon "\u03A5" Phi "\u03A6"\
+	  Chi "\u03A7" Psi "\u03A8" Omega "\u03A9" alpha "\u03B1"\
+	  beta "\u03B2" gamma "\u03B3" delta "\u03B4" epsilon "\u03B5"\
+	  zeta "\u03B6" eta "\u03B7" theta "\u03B8" iota "\u03B9"\
+	  kappa "\u03BA" lambda "\u03BB" mu "\u03BC" nu "\u03BD" xi "\u03BE"\
+	  omicron "\u03BF" pi "\u03C0" rho "\u03C1" sigmaf "\u03C2"\
+	  sigma "\u03C3" tau "\u03C4" upsilon "\u03C5" phi "\u03C6"\
+	  chi "\u03C7" psi "\u03C8" omega "\u03C9" thetasym "\u03D1"\
+	  upsih "\u03D2" piv "\u03D6" ensp "\u2002" emsp "\u2003"\
+	  thinsp "\u2009" zwnj "\u200C" zwj "\u200D" lrm "\u200E"\
+	  rlm "\u200F" ndash "\u2013" mdash "\u2014" lsquo "\u2018"\
+	  rsquo "\u2019" sbquo "\u201A" ldquo "\u201C" rdquo "\u201D"\
+	  bdquo "\u201E" dagger "\u2020" Dagger "\u2021" bull "\u2022"\
+	  hellip "\u2026" permil "\u2030" prime "\u2032" Prime "\u2033"\
+	  lsaquo "\u2039" rsaquo "\u203A" oline "\u203E" euro "\u20AC"\
+	  trade "\u2122" larr "\u2190" uarr "\u2191" rarr "\u2192"\
+	  darr "\u2193" harr "\u2194" crarr "\u21B5" forall "\u2200"\
+	  part "\u2202" exist "\u2203" empty "\u2205" nabla "\u2207"\
+	  isin "\u2208" notin "\u2209" ni "\u220B" prod "\u220F"\
+	  sum "\u2211" minus "\u2212" lowast "\u2217" radic "\u221A"\
+	  prop "\u221D" infin "\u221E" ang "\u2220" and "\u2227"\
+	  or "\u2228" cap "\u2229" cup "\u222A" int "\u222B" there4 "\u2234"\
+	  sim "\u223C" cong "\u2245" asymp "\u2248" ne "\u2260"\
+	  equiv "\u2261" le "\u2264" ge "\u2265" sub "\u2282" sup "\u2283"\
+	  nsub "\u2284" sube "\u2286" supe "\u2287" oplus "\u2295"\
+	  otimes "\u2297" perp "\u22A5" sdot "\u22C5" lceil "\u2308"\
+	  rceil "\u2309" lfloor "\u230A" rfloor "\u230B" loz "\u25CA"\
+	  spades "\u2660" clubs "\u2663" hearts "\u2665" diams "\u2666"]
+
+	while {[set i1 [string first "&" $str $i0]] != -1} {
+		append res [string range $str $i0 $i1-1]
+		set esc [string range $str $i1 [string first ";" $str $i1]]
+		if {[string equal -length 3 $esc "&#x"]} {
+			append res [format %c [scan [string range $esc 3 end-1] %x]]
+		} elseif {[string equal -length 2 $esc "&#"]} {
+			append res [format %c [string range $esc 2 end-1]]
+		} else {
+			append res [dict get $_html_unescape_escseq\
+			  [string range $esc 1 end-1]]
+		}
+		set i0 [expr {$i1 + [string length $esc]}]
+	}
+	if {$i0 < [string length $str]} {
+		append res "[string range $str $i0 end]"
+	}
+
+	return $res
+}
